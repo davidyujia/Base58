8c13e53db5b7ea210de515270760cb0e310106a9
David <davidyu@kingwaytek.com>
2019-07-01 10:25:17 +0800

    base58

---
+++ davidyujia.Base58.Tests/Base58Tests.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace davidyujia.Base58.Tests
+{
+    [TestClass]
+    public class Base58Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testString = "Hello";
+
+            var bytes = Encoding.UTF8.GetBytes(testString);
+
+            var encode = davidyujia.Base58.Base58.Encode(bytes);
+
+            Assert.AreEqual("9Ajdvzr", encode);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                var testString = GetRandomString(i);
+
+                var bytes = Encoding.UTF8.GetBytes(testString);
+
+                var encode = davidyujia.Base58.Base58.Encode(bytes);
+
+                var decode = davidyujia.Base58.Base58.Decode(encode);
+
+                var str = Encoding.UTF8.GetString(decode);
+
+                Assert.AreEqual(testString, str);
+            }
+        }
+
+        public static string GetRandomString(int length)
+        {
+            var str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            var next = new Random();
+            var builder = new StringBuilder();
+            for (var i = 0; i < 5; i++)
+            {
+                builder.Append(str[next.Next(0, str.Length)]);
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ davidyujia.Base58/Base58.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace davidyujia.Base58
+{
+    public sealed class Base58
+    {
+        private static readonly char[] base58Table = {
+            '1','2','3','4','5','6','7','8','9',
+            'A','B','C','D','E','F','G','H','J',
+            'K','L','M','N','P','Q','R','S','T',
+            'U','V','W','X','Y','Z','a','b','c',
+            'd','e','f','g','h','i','j','k','m',
+            'n','o','p','q','r','s','t','u','v',
+            'w','x','y','z' };
+
+        public static string Encode(byte[] data)
+        {
+            var sum = data.Aggregate<byte, BigInteger>(0, (current, b) => current * 256 + b);
+
+            var sb = new StringBuilder();
+            do
+            {
+                sum = BigInteger.DivRem(sum, 58, out var remainder);
+                sb.Append(base58Table[(int)remainder]);
+            } while (sum != 0);
+
+            return new string(sb.ToString().Reverse().ToArray());
+        }
+
+        public static byte[] Decode(string encryptedString)
+        {
+            var sum = encryptedString.ToArray().Aggregate<char, BigInteger>(0, (current, c) => (current * 58) + Array.IndexOf(base58Table, c));
+
+            var bit8 = 0;
+            var list = new List<byte>();
+            do
+            {
+                var twoPower = BigInteger.Pow(2, 8 * bit8++);
+                var remainder = sum % (twoPower * 256);
+                var quotient = remainder / twoPower;
+                sum -= remainder;
+                list.Add((byte)quotient);
+            } while (sum != 0);
+            list.Reverse();
+            return list.ToArray();
+        }
+    }
+}
